43b8dab0f2e4bd6c66c6a1890856afdccc287433
TrevisGreen <Trevis.Green@gmail.com>
2023-04-25 11:58:00 -0500

    Added functionality to show the login page

--- src/main/java/com/example/basicloginusingspringbootsecurity/controller/LoginController.java
+++ src/main/java/com/example/basicloginusingspringbootsecurity/controller/LoginController.java
@@ -1,4 +1,12 @@
 package com.example.basicloginusingspringbootsecurity.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginController {
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage() {
+        return "login.html";
+    }
 }
