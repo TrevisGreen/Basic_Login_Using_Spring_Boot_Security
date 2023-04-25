b3f96e9bafc5afab767c123234b9cc78fa31b853
TrevisGreen <Trevis.Green@gmail.com>
2023-04-25 11:51:43 -0500

    Initial commit

---
+++ src/main/java/com/example/basicloginusingspringbootsecurity/BasicloginusingspringbootsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicloginusingspringbootsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicloginusingspringbootsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicloginusingspringbootsecurityApplication.class, args);
+	}
+
+}
